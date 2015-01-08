ee2f37d94863ebf00e187157ea6cb0394b2f44be
gonzaponte <gonzaponte@gmail.com>
2015-01-08 21:05:30 +0100

    Filling macro

---
+++ v1/Fill.C
@@ -0,0 +1,23 @@
+
+
+void Fill( )
+{
+    gROOT->ProcessLine(".L GTrack.h++");
+    gROOT->ProcessLine(".L GTrack.cxx++");
+    
+    GInfo::GTrack* _gtrack = 0;
+    TFile* tf = new TFile("filled.root","recreate");
+    TTree* tt = new TTree("GTree","GTree");
+    TTree::SetBranchStyle(1);
+    GTree->Branch("GTracks","GInfo::GTrack",&_gtrack);
+    for (int i=0);i<10;i++)
+    {
+        _gtrack = new GInfo::GTrack();
+        tt->Fill();
+    }
+    tt->Write();
+    tf->Close();
+    delete _gtrack;
+    delete tt;
+    delete tf;
+}
